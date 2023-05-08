d83f80ec12923b61aa683568b5b268da5788d455
deepaks2293 <singhdeepaks2293@gmail.com>
2023-05-08 20:30:33 +0530

    Completed Paint Program added if else to catch err

--- Head_First_Go/Functions/paint.go
+++ Head_First_Go/Functions/paint.go
@@ -2,21 +2,29 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of  %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of  %0.2f is invalid", height)
+	}
 	area := width * height
 	//fmt.Printf("%.2f liters needed\n", area/10.0)
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
 	var total, amount float64
-	amount = paintNeeded(4.2, 3.0)
-	fmt.Printf("%0.2f liters needed\n",amount)
-	total += amount
-	amount = paintNeeded(5.2, 3.5)
-	fmt.Printf("%0.2f liters needed\n",amount)
-	total += amount
-	fmt.Printf("%0.2f liters needed\n",total)
+	var err error
+	amount, err = paintNeeded(4.2, 3.0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%0.2f liters needed\n", amount)
+		total += amount
+		fmt.Printf("Total: %0.2f liters needed\n", total)
+	}
 
 }
 
@@ -29,5 +37,10 @@ func main() {
 3) Adding var to store amount, total as float64
 
 4) Add amount in total and printf at the end
+
+5) Added multiple return funtion (float64 and error)
+
+6) In main funtion added if else loop to catch error.const
+
 
 */
