887a48796866e997be6b28526ca33d7afba06101
deepaks2293 <singhdeepaks2293@gmail.com>
2023-05-10 15:02:19 +0530

    Go Program for FtoC

---
+++ Head_First_Go/Package/tocelcius.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+func main() {
+	fmt.Print("Enter a temprature in Fahrenheit: ")
+	farhrenheit, err := getFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	celcius := (farhrenheit - 32) * 5 / 9
+	fmt.Printf("%0.2f degrees celcisus\n", celcius)
+}
